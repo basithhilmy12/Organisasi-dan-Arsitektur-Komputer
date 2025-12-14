23542644c9c152b2bfd475792f7572991bd8a037
Basith <0110225088@student.nurulfikri.ac.id>
2025-12-14 15:11:28 +0700

    Add files via upload

---
+++ program.py
@@ -0,0 +1,1 @@
+print("Teknik Informatika")
---
+++ tugasB.asm
@@ -0,0 +1,4 @@
+LOAD 14     ; ACC ← isi memori 14 (2)
+ADD 15      ; ACC ← ACC + isi memori 15 (2 + 3 = 5)
+STORE 13    ; Memori 13 ← ACC (5)
+HALT        ; Program berhenti
